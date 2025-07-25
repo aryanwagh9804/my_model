7a34794dcffcbfdb783d780b1a97735edf022b08
aryanwagh9804 <backupaditya7104@gmail.com>
2025-07-25 20:58:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def ask():
     prompt = data.get('prompt', '')
 
     # Send request to your local Ollama running at home using ngrok or other tunnel
-    response = requests.post('https://32b586d98207.ngrok-free.app/api/generate', json={"prompt": prompt})
+    response = requests.post(' https://19cd6a723517.ngrok-free.app/api/generate', json={"prompt": prompt})
     return jsonify(response.json())
 
 if __name__ == '__main__':
