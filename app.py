ee605c17b9e017a780d5b84c6cfa19b6330f8388
aryanwagh9804 <backupaditya7104@gmail.com>
2025-07-25 21:54:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+import requests
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+    
 @app.route('/ask', methods=['POST'])
 def ask():
     data = request.json
